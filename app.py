ff24001eb8a84f6279eb1ac9ca13cf05aeaff660
Avijit Ghosh <avijitg22@gmail.com>
2016-03-31 09:07:08 +0530

    Changes

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from werkzeug import secure_filename
 import os
+import time
+# timestr = time.strftime("%Y%m%d-%H%M%S")
 
 app = Flask(__name__)
 
@@ -14,20 +16,45 @@ def allowed_file(filename):
 	return '.' in filename and \
 		   filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
+def filesave(flist):
+	uploaded_files = request.files.getlist(flist)
+	print uploaded_files
+
+	for file in uploaded_files:
+		if file and allowed_file(file.filename):
+			# filename = secure_filename(file.filename)
+			filename=time.strftime("%Y%m%d-%H%M%S")+".txt"
+			absolute_path = os.path.abspath(UPLOAD_FOLDER+filename)
+			file.save(absolute_path)
+		#return 'file uploaded successfully'
+
+
 
 @app.route("/")
 def main():
 	return render_template('index.html')
+
+@app.route('/summary')
+def summary():
+	return render_template('index2.html')
+
+# Route that will process the file upload
+@app.route('/uploader2', methods=['POST'])
+def upload2():
+	filesave('file[]');
+	return render_template('processing.html')
 
 # Route that will process the file upload
 @app.route('/uploader', methods=['POST'])
 def upload():
 	file = request.files['file']
 	if file and allowed_file(file.filename):
-		filename = secure_filename(file.filename)
+		# filename = secure_filename(file.filename)
+		filename=time.strftime("%Y%m%d-%H%M%S")+".txt"
 		absolute_path = os.path.abspath(UPLOAD_FOLDER+filename)
 		file.save(absolute_path)
 		return 'file uploaded successfully'
+
 		# return redirect(url_for('uploaded_file',
 		#                         filename=filename))
 	return render_template('processing.html')
