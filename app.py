bb103316aea4a852fb3b82d6469277b8002a8422
AvijitGhosh82 <avijitg22@gmail.com>
2016-04-02 17:42:51 +0530

    Text view

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect,url_for
 from werkzeug import secure_filename
 import os
 
 app = Flask(__name__)
+
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = os.path.join(APP_ROOT, 'uploads/')
@@ -19,6 +20,15 @@ def allowed_file(filename):
 def main():
 	return render_template('index.html')
 
+@app.route("/classify", methods=['GET'])
+def classify():
+	print "howmow"
+	return render_template('classify.html')
+
+@app.route("/summarize", methods=['GET'])
+def summarize():
+	return render_template('summarize.html')
+
 # Route that will process the file upload
 @app.route('/uploader', methods=['POST'])
 def upload():
@@ -28,18 +38,16 @@ def upload():
 		absolute_path = os.path.abspath(UPLOAD_FOLDER+filename)
 		file.save(absolute_path)
 		return 'file uploaded successfully'
-		# return redirect(url_for('uploaded_file',
-		#                         filename=filename))
-	return render_template('processing.html')
+
+	with open("output/abcd.txt", "r") as f:
+		content = f.read()
+	return render_template("processing.html", content=content)
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
 	return send_from_directory(app.config['UPLOAD_FOLDER'],
 							   filename)
 
-@app.route("/b")
-def bu():
-	return "Bubla"
 
 if __name__ == "__main__":
 	app.debug = True
